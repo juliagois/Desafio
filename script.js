a2e23bedf06f9b231c8e7c481e387a0a4443fa8f
julia <julia27354@gmail.com>
2019-04-21 12:15:57 -0300

    criar menu hamburguer

---
+++ script.js
@@ -0,0 +1,13 @@
+let hamburguer= document.querySelector("div");
+let nomes= document.querySelector("#menuHamburguer");
+//let links = document.querySelector("a");
+
+
+
+function abrirMenu(){
+    nomes.classList.toggle("abrirMenu");
+ }
+  
+  menuHamburguer.onclick = abrirMenu;
+
+
